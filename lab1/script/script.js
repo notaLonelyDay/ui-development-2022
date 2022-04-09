75af7eafcc7d506d0e0e18c4d3511abb2e365034
alexb <alexbondar03@gmail.com>
2022-04-09 17:52:43 +0300

    спойлеры теперь сохраняют состояние после перезагрузки

--- lab1/script/script.js
+++ lab1/script/script.js
@@ -1,15 +1,37 @@
-const blocks = document.querySelectorAll(".FAQ-block");
-let preventActiveElement = blocks[0];
 
+const blocks = document.querySelectorAll(".FAQ-block");
 
 blocks.forEach(function (item) {
     item.addEventListener("click", blockClick);
 });
 
+let openIndex = getCookie("openIndex");
+let firstElement = blocks[openIndex ?? 0];
+let preventActiveElement;
+
+firstElement.click();
+
 function blockClick() {
     if (this.classList.contains("hidden")) {
+
         this.classList.toggle("hidden");
-        preventActiveElement.classList.toggle("hidden");
+        preventActiveElement?.classList.toggle("hidden");
         preventActiveElement = this;
+
+        let index;
+        blocks.forEach((item, num) => {
+            if (item === this) {
+                index = num;
+            }
+        })
+
+        document.cookie = "openIndex=" + index;
     }
+}
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
