68136b9daf7279f38409ab49251f40317eb2fa0b
nikita.artikhovich <nikita.artikhovich@gmail.com>
2022-04-24 22:28:33 +0300

    ..

--- lab1/script/script.js
+++ lab1/script/script.js
@@ -225,13 +225,20 @@ const reviews = [
         stars: 5,
     },
     {
-        name: "Allah akbar",
-        position: "Head of Bulba",
+        name: "Alexey Y.",
+        position: "Head of Arc",
         title: "Really bad app",
         picture_src: "img/reviews-people/man3.png",
-        text: `Shugarbelle is a sugar-free, gluten-free, dairy-free, egg-free, nut-free,\n" +
-            "and vegan-friendly snack that is made with the finest ingredients.`,
+        text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.`,
         stars: 1,
+    },
+    {
+        name: "Alexey Ya.",
+        position: "Backend developer",
+        title: "Perfecto amigos",
+        picture_src: "img/reviews-people/man1.png",
+        text: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.`,
+        stars: 4,
     },
 ];
 
